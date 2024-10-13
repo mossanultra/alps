b66a9457cd1b08fd1aa4e9cb8f90877ca73a66ec
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-10-13 07:04:28 +0000

    PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextPWA = require('next-pwa');
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,12 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>にんにく</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#a8a6a3" />
+      </Head>
       <p>tairaの部屋</p>
       <p>準備中だよ</p>
       <h1>ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄</h1>
