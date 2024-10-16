0aa97873cddf5007eddd9dcac3f849897e53c61e
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-10-16 17:42:29 +0900

    CHIPも入れてみた

--- pages/components/card/card.tsx
+++ pages/components/card/card.tsx
@@ -14,6 +14,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Chip } from '@mui/material';
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -76,6 +77,9 @@ export default function RecipeReviewCard() {
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
         ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄ﾆﾝ(ง ˘ω˘ )วﾆｸ🧄
         </Typography>
+        <Chip label="primary" color="primary" />
+        <Chip label="success" color="success" />
+        <Chip label="secondary" color="secondary" />
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
