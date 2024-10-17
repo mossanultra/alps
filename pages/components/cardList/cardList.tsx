4211e3032ca50aac5414f4833e192a428194627f
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-10-17 22:59:56 +0900

    Listを中央配置

--- pages/components/cardList/cardList.tsx
+++ pages/components/cardList/cardList.tsx
@@ -3,13 +3,15 @@ import RecipeReviewCard from "../card/card";
 
 export default function CardList() {
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <ListItem alignItems="flex-start">
-        <RecipeReviewCard></RecipeReviewCard>
-      </ListItem>
-      <ListItem alignItems="flex-start">
-        <RecipeReviewCard></RecipeReviewCard>
-      </ListItem>
-    </List>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <ListItem alignItems="flex-start">
+          <RecipeReviewCard></RecipeReviewCard>
+        </ListItem>
+        <ListItem alignItems="flex-start">
+          <RecipeReviewCard></RecipeReviewCard>
+        </ListItem>
+      </List>
+    </div>
   );
 }
