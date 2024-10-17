4d954b8a6f42ff11cc2fb939d782c6d660426367
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-10-17 10:33:50 +0900

    List

--- pages/components/otokoume/otokoume.tsx
+++ pages/components/otokoume/otokoume.tsx
@@ -1,25 +1,32 @@
 import classes from "./otokoume.module.css";
 import HoloEffectCard from "../holocard/holoEffectCard";
+import { List, ListItem } from "@mui/material";
 
 export default function Otokoume() {
   return (
     <div className={classes.canvas_container}>
-      <div className={classes.container}>
-        <HoloEffectCard
-          image={"otokoume.jpg"}
-          position={[0, 0, 0]}
-          className={classes.canvas}
-          enableOrbitContorls={true}
-        ></HoloEffectCard>
-      </div>
-      <div className={classes.container}>
-      <HoloEffectCard
-          image={"china.jpg"}
-          position={[0, 0, 0]}
-          className={classes.canvas}
-          enableOrbitContorls={true}
-        ></HoloEffectCard>
-        </div>
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <ListItem alignItems="flex-start">
+          <div className={classes.container}>
+            <HoloEffectCard
+              image={"otokoume.jpg"}
+              position={[0, 0, 0]}
+              className={classes.canvas}
+              enableOrbitContorls={true}
+            ></HoloEffectCard>
+          </div>
+        </ListItem>
+        <ListItem alignItems="flex-start">
+          <div className={classes.container}>
+            <HoloEffectCard
+              image={"china.jpg"}
+              position={[0, 0, 0]}
+              className={classes.canvas}
+              enableOrbitContorls={true}
+            ></HoloEffectCard>
+          </div>
+        </ListItem>
+      </List>
     </div>
   );
 }
