591c9ceb6a747a39c792abdcda3be07644bb20da
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-10-20 11:14:24 +0900

    fix build error

--- pages/components/card/card_smanu.tsx
+++ pages/components/card/card_smanu.tsx
@@ -1,22 +1,11 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Chip, Grid } from "@mui/material";
-import styles from "../card.module.css";
-import BasicTable from "../table/table";
 import { Article } from "../../api/articles/articles";
 
 interface ExpandMoreProps extends IconButtonProps {
@@ -54,6 +43,9 @@ export default function CardSmanu(props: ExpandMoreProps) {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  if (!props.article) {
+    return null; // 何も表示しない、またはローディング状態にするなど
+  }
 
   return (
     <Card sx={{ display: "flex", width: "100%", maxWidth: 600 }}>
