ea0e8d0a9189990f48d92485115c56280e0ced56
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-10-20 10:22:28 +0900

    色々修正

--- pages/components/cardList/cardList.tsx
+++ pages/components/cardList/cardList.tsx
@@ -1,21 +1,44 @@
 import { List, ListItem } from "@mui/material";
 import RecipeReviewCard from "../card/card";
+import { useEffect, useState } from "react";
+import { Article } from "../../types/article";
+import CardSmanu from "../card/card_smanu";
 
 export default function CardList() {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  async function getArticles() {
+    const response = await fetch("/api/articles/list");
+    const articles = await response.json();
+    setArticles(articles);
+  }
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      await getArticles();
+      setIsLoading(false)
+    };
+    fetchArticles();
+  }, []); 
+
   return (
     <div
       style={{ display: "flex", justifyContent: "center", marginTop: "72px" }}
     >
       <List sx={{ width: "100%", maxWidth: 720, bgcolor: "background.paper" }}>
-        <ListItem alignItems="flex-start">
-          <RecipeReviewCard expand={false} title={"mozuku"} image={'/china.jpg'}></RecipeReviewCard>
-        </ListItem>
-        <ListItem alignItems="flex-start">
-          <RecipeReviewCard expand={false} title={"taira"} image={'/hatiware.jpeg'}></RecipeReviewCard>
-        </ListItem>
-        <ListItem alignItems="flex-start">
-          <RecipeReviewCard expand={false} title={"mekabu"} image={'/otokoume.jpg'}></RecipeReviewCard>
-        </ListItem>
+        {!isLoading && articles.map((a) => {
+          return (
+            <>
+              <ListItem alignItems="flex-start">
+                <CardSmanu
+                  expand={false}
+                  title={a.title}
+                  image={"/china.jpg"} 
+                  content={a.content}                ></CardSmanu>
+              </ListItem>
+            </>
+          );
+        })}
       </List>
     </div>
   );
