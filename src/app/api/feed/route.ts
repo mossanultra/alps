4386ccca5435cf1dd80144f3ef273764a19b9862
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-10-23 14:39:57 +0900

    deployするためのpush

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -41,6 +41,7 @@ export async function GET() {
     });
 
     return NextResponse.json(allFeedItems);
+    
   } catch (error) {
     return NextResponse.json({ message: `${error}` }, { status: 500 });
   }
