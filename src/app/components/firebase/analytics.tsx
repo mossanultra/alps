a1a69a97485a5640fea54f39272f0109b6c9ef8e
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-11-08 21:30:27 +0900

    fix build error

--- src/app/components/firebase/analytics.tsx
+++ src/app/components/firebase/analytics.tsx
@@ -1,16 +1,13 @@
 'use client'
-// This component is to initialize Firebase Anaytics (in a client component).
+// This component is to initialize Firebase Analytics (in a client component).
 // And it is supposed to be used in src/app/layout.tsx.
-//
-// You can still send an event without it.
-// And the user will be tracked as soon as they send an event.
-// However, active users (in real time) will not be tracked without it before they send an event.
+
 import { analytics } from '../../../firebase'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
 analytics
 
 const FirebaseAnalytics = () => {
-  // dummy JSX component
   return <div></div>
 }
 
