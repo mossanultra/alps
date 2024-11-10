3ca43ecaf2e27b8a349be9c3a73f72c61e05010f
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-11-10 21:29:09 +0900

    ♡

--- src/app/components/tairaimage/content-card/content-card.tsx
+++ src/app/components/tairaimage/content-card/content-card.tsx
@@ -1,5 +1,4 @@
-// Card.tsx
-import React from "react";
+import React, { useState } from "react";
 import styles from "./content-card.module.css";
 
 type ContentCardProps = {
@@ -11,12 +10,29 @@ type ContentCardProps = {
   onClick: () => void;
 };
 
-const ContentCard: React.FC<ContentCardProps> = ({ post, onClick }) => (
-  <div className={styles.card2} onClick={onClick}>
-    <img src={post.imageBase64} alt={post.text} className={styles.cardImage2} />
-    <div className={styles.cardText}>{post.text}</div>
-    <div className={styles.cardSubtext}>山口県&nbsp;萩市</div>
-  </div>
-);
+const ContentCard: React.FC<ContentCardProps> = ({ post, onClick }) => {
+  const [likeCount, setLikeCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = (event: React.MouseEvent) => {
+    event.stopPropagation(); // onClickの伝播を防ぐ
+    setLiked(!liked);
+    setLikeCount(prevCount => prevCount + (liked ? -1 : 1));
+  };
+
+  return (
+    <div className={styles.card2} onClick={onClick}>
+      <img src={post.imageBase64} alt={post.text} className={styles.cardImage2} />
+      <div className={styles.cardText}>{post.text}</div>
+      <div className={styles.cardSubtext}>山口県&nbsp;萩市</div>
+      <div className={styles.likeContainer}>
+        <button className={styles.likeButton} onClick={handleLike}>
+          {liked ? "❤️" : "♡"}
+        </button>
+        <span className={styles.likeCount}>{likeCount}</span>
+      </div>
+    </div>
+  );
+};
 
 export default ContentCard;
