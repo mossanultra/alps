ed8bec8c20f434d1e1467c59ea2318ac68b47522
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-11-10 20:46:51 +0900

    fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import FirebaseAnalytics from "./components/firebase/analytics";
 import { usePathname } from "next/navigation";
-import { useTransition, animated } from "@react-spring/web";
+// import { useTransition, animated } from "@react-spring/web";
 import React from "react";
 
 const uzura = localFont({
@@ -17,13 +17,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
+  // const pathname = usePathname();
 
-  const transitions = useTransition(pathname, {
-    from: { opacity: 0, transform: "translateX(100%)" },
-    enter: { opacity: 1, transform: "translateX(0%)" },
-    leave: { opacity: 0, transform: "translateX(-100%)" },
-  });
+  // const transitions = useTransition(pathname, {
+  //   from: { opacity: 0, transform: "translateX(100%)" },
+  //   enter: { opacity: 1, transform: "translateX(0%)" },
+  //   leave: { opacity: 0, transform: "translateX(-100%)" },
+  // });
 
   return (
     <html lang="en">
