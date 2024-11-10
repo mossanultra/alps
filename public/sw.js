6194b2d2bbf615de8180d904a1d288782ca57277
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-11-10 15:37:25 +0900

    キャッシュ戦略の見直し

--- public/sw.js
+++ public/sw.js
@@ -9,8 +9,6 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
