f459938b04385cf7c1e7c33b025e7a46b64c085d
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-11-13 02:36:21 +0900

    fix build error

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export interface Chat {
 // 登録されている座標のリスト
 export async function GET() {
   try {
-    const _chats: Chat[] = chats.map((m: { userName: any; userIcon: any; text: any; }) => {
+    const _chats: Chat[] = chats.map((m) => {
       return({userName: m.userName,userIcon: m.userIcon, text: m.text})
     })
 
