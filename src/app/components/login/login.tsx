8aabc8d029cc68eb226f31cdd306cc761010305f
SugimotoShinya <95908731+SugimotoShinya@users.noreply.github.com>
2024-11-25 07:46:22 +0900

    ログイン画面の改善

--- src/app/components/login/login.tsx
+++ src/app/components/login/login.tsx
@@ -1,51 +1,46 @@
+// app/page.tsx
+"use client";
 import { useAuthContext } from "@/app/context/AuthContext";
 import { useState } from "react";
+import styles from "./login.module.css";
 
-export default function AuthView() {
-  const [isSingUp, setIsSigneUp] = useState(false);
+export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { SignIn, SignUp } = useAuthContext();
-
-  const handleAuth = async () => {
-    if (isSingUp) {
-      SignUp(email, password);
-    } else {
-      SignIn(email, password);
-    }
+  const handleSignIn = () => {
+    SignIn(email, password);
   };
-  return (
-    <div>
-      <h1>{isSingUp ? "サインアップ" : "サインイン"}</h1>
-      <div>
-        <label>
-          メールアドレス：
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          パスワード
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-      </div>
-      <button onClick={handleAuth}>
-        {isSingUp ? "サインアップ" : "サインイン"}
-      </button>
 
-      <button onClick={() => setIsSigneUp(!isSingUp)}>
-        {isSingUp
-          ? "アカウントを持ってる場合はサインインへ"
-          : "アカウントがない場合はサインアップへ"}
-      </button>
+  const handleSignUp = () => {
+    SignUp(email, password);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.pandaIcon}></div>
+      <h1 className={styles.title}>Welcome</h1>
+      <div className={styles.form}>
+        <input
+          type="email"
+          placeholder="メールアドレス"
+          className={styles.input}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="パスワード"
+          className={styles.input}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className={styles.button} onClick={handleSignIn}>
+          ログイン
+        </button>
+        <button className={styles.button} onClick={handleSignUp}>
+          新規登録
+        </button>
+      </div>
+      <p className={styles.footer}>© 2024 Maho&Mozuku</p>
     </div>
   );
 }
